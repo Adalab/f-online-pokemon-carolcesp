36255df5cd2f8e2bf6b0c50519adb068561c9926
carolcesp <carolinacespedes90@gmail.com>
2019-03-09 14:44:05 +0100

    filter ok, changes in the pokemonList and try to order pokemon

--- src/App.js
+++ src/App.js
@@ -18,36 +18,48 @@ class App extends Component {
     this.getPokemon();
   }
 
+  saveLocalStorage(idNumber, myPokemon){
+    localStorage.setItem(myPokemon,JSON.stringify(idNumber))
+  }
+  
+  getSavedLocalStorage(){
+    if(localStorage.getItem('poke') !== null){
+      const savePokemon = JSON.parse(localStorage.getItem('poke'));
+      this.setState({
+        myPokemon: savePokemon,
+      })
+    } else {
+      this.getPokemon();
+    }
+  }
+
   getPokemon() {
     getApiPokemon()
     .then(poke => {
-      console.log('punto1',poke)
-
-//realizo map de results para acceder a la url que devuelve la Api
-      const pokeUrl = poke.results.map(item => {
-          return item.url
+      const addId = poke.results.map((item,index) => {
+        return {...item,id:index}
       });
+      console.log('añado id a poke >',addId)
+      const pokeUrl = addId.map(item => {
+        return item.url
+      });
+      
       for (let i = 0; i < pokeUrl.length; i++){
         fetch(pokeUrl[i])
           .then(response => response.json())
-          .then(pokeInfo => {
-
-            console.log('punto2',pokeInfo)
-
-  //creo array para ir metiendo los tipos una vez recorrido
+          .then(pokeUrl => { 
+        
             const pokeType = []; 
-              for (let i = 0; i < pokeInfo.types.length; i++) {
-                pokeType.push(pokeInfo.types[i].type["name"]);
+              for (let i = 0; i < pokeUrl.types.length; i++) {
+                pokeType.push(pokeUrl.types[i].type.name);
               }
-  //en mi obj poke meto las propiedades que necesito
             const poke = {
-              pokeName: pokeInfo.name,
-              pokeId: pokeInfo.id,
-              pokeImg: pokeInfo.sprites.front_default,
+              pokeName: pokeUrl.name,
+              pokeId:pokeUrl.id,
+              pokeImg: pokeUrl.sprites.front_default,
               type: pokeType
             };
-            console.log('Que devuelve poke?? >',poke)
-  //constante que accede a myPokemon y mete las propiedad de la constante anterior poke!
+            console.log('que devuelve poke? >',poke)
             const resultInfo = this.state.myPokemon;
             resultInfo.push(poke);
             this.setState({
@@ -57,21 +69,6 @@ class App extends Component {
         })
       }
     })
-  }
-
-  saveLocalStorage(poke, myPokemon){
-    localStorage.setItem(myPokemon,JSON.stringify(poke))
-  }
-  
-  getSavedLocalStorage(){
-    if(localStorage.getItem('myPokemon') !== null){
-      const savePokemon = JSON.parse(localStorage.getItem('myPokemon'));
-      this.setState({
-        myPokemon: savePokemon,
-      })
-    } else {
-      this.getPokemon();
-    }
   }
 
   getFieldName(e){
@@ -86,14 +83,12 @@ class App extends Component {
       .filter(item => {
       const name = item.pokeName;
       return (name.toUpperCase().includes(this.state.fieldName.toUpperCase())) ? true : false;
-     })
+    })
     return filterName;
   };
 
-
   render() {
-   
-    const {myPokemon} = this.state;
+    const filterPoke = this.filterPokemon();
     return (
       <div className="page__wrapper">
         <header className="header__content">
@@ -101,7 +96,7 @@ class App extends Component {
         </header>
         <main className="main__content">
           <FilterNamePoke name={this.getFieldName}/>
-          <PokemonList results={myPokemon} /> 
+          <PokemonList filterPoke={filterPoke}/> 
         </main>
       </div>
     );
