747142d2e5876f290119accfaf181e2d10aab7d6
carolcesp <carolinacespedes90@gmail.com>
2019-03-10 21:25:59 +0100

    add folder docs to github pages

---
+++ docs/precache-manifest.7feec134a0f408905b7af602285b4bce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d63ec6529a4494a3596",
+    "url": "./static/css/main.3b1c5d8d.chunk.css"
+  },
+  {
+    "revision": "9d63ec6529a4494a3596",
+    "url": "./static/js/main.9d63ec65.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "94b465cf7edd69e53c64",
+    "url": "./static/js/2.94b465cf.chunk.js"
+  },
+  {
+    "revision": "4eb99bfad2337fc7e65b74d12ba2b920",
+    "url": "./index.html"
+  }
+];
